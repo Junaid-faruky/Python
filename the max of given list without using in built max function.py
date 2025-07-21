a0eae8ed694f7cbaba56afc2f20906ba31acd4d3
Junaid faruky <167950564+Junaid-faruky@users.noreply.github.com>
2025-07-21 22:43:28 +0530

    Create the max of given list without using in built max function.py

---
+++ the max of given list without using in built max function.py
@@ -0,0 +1,9 @@
+#find the max of given list without using in built max function
+list_1=[5,8,76,34,13,54]
+max_num=list_1[0]
+
+fir i in range (1,len(list_a)):
+    if(list_a[i]>max_num):
+        max_num=list_a[i]
+
+print(max_num)
